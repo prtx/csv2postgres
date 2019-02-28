a53176a745ce79593597da03e5549d4d526154e1
prtx <prtx.pratikshrestha@gmail.com>
2019-02-28 22:59:30 +0545

    Add options to insert

--- csv2pg.py
+++ csv2pg.py
@@ -34,14 +34,14 @@ def create_db_if_not_exists(database):
 TYPE_DICT = {"str": "varchar", "int": "bigint", "float": "float"}
 
 
-def csv_to_table(file_path, skip_line=0, insert_till=-1):
+def csv_to_table(file_path, delimiter, skip_line=0, insert_till=-1):
     with open(file_path) as csv_file:
         # skip line till header
         for _ in range(skip_line):
             next(csv_file)
 
         # read file and header
-        reader = csv.reader(csv_file, delimiter=";")
+        reader = csv.reader(csv_file, delimiter=delimiter)
         headers = [x.split("|") for x in next(reader)]
 
         # create table
@@ -77,6 +77,9 @@ def parse_args(args=None):
     """
     parser = argparse.ArgumentParser(description="CSV to postgres table.")
     parser.add_argument("csv", type=str, help="CSV file path")
+    parser.add_argument("-d", "--delimiter", type=str, help="Delimiter")
+    parser.add_argument("-sl", "--skip-line", type=int, help="Skip line till header")
+    parser.add_argument("-it", "--insert-till", type=int, help="Insert till line number")
 
     # if arguments passed as parameters then parse them else not
     if args:
@@ -85,8 +88,14 @@ def parse_args(args=None):
 
 
 if __name__ == "__main__":
-    args = parse_args()
     create_db_if_not_exists(settings.DB_NAME)
+
     connection = create_connection(settings.DB_NAME)
     cursor = connection.cursor()
-    csv_to_table(args.csv, skip_line=2, insert_till=100)
+
+    args = parse_args()
+    kwargs = {k: v for k, v in vars(args).items() if v}
+    csv_file = kwargs.pop("csv")
+    delimiter = kwargs.pop("delimiter")
+
+    csv_to_table(csv_file, delimiter, **kwargs)
